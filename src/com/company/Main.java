c0c732e53d8021dec94f0540c8006f96d0875f02
lewein <55996926+lewein@users.noreply.github.com>
2019-10-12 16:51:30 +0300

    Update Main.java
    Java: поиск вхождения каждого слова в предложение

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
+//Поиск вхождения каждого слова с предложение
 package com.company;
+import java.util.*;
+import java.lang.String;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.print("Введите предложение\n");
+        Scanner sentense = new Scanner(System.in);
+        String words = sentense.nextLine();
+        ArrayList<String> check = new ArrayList<>(words.length());
+        for (String word : words.split(" ")) {
+            if (check.indexOf(word) == -1) {
+                check.add(word);
+                System.out.println(Count(word, words) + String.format(" %s", word));
+            }
+        }
     }
+
+    private static int Count(String word, String words) {
+        int cnt = 0;
+        for (String el : words.split(" ")) {
+            if (el.equals(word)) {
+                cnt ++;
+            }
+        }
+        return cnt;
+    }
+
 }
